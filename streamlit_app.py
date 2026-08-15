b0d5b9a0c5bb276d707af1a57a31adff444983fb
feargodemma234 <feargodemma56@gmail.com>
2026-08-15 11:33:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,4 +55,4 @@ else:
     st.info("Upload a CSV to start. Template: aisle, time_min, order_id, worker, shift, item_count")
 
 st.markdown("---")
-st.link_button("📧 Book Free Demo", "mailto:yourname@tqalogistics.com")
+st.link_button("📧 Book Free Demo", "mailto:"quantumindustries258@gmail.com")
